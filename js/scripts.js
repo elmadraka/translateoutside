207b8fbdbbdbc6238acd6858294b15b8cba75e63
dani <dani@crocantickets.com>
2025-04-23 16:54:08 +0200

    support for more files inside data folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,114 @@
 let allFiles = []
+const keysForFile = { 
+  "Actors.json": ['id:int', 'name:string', 'nickname:string'],
+  "Armors.json": ['id:int', 'name:string', 'description:string'],
+  "Classes.json": ['id:int', 'name:string', 'note:string'],
+  "Enemies.json": ['id:int', 'name:string'],
+  "Items.json": ['id:int', 'name:string', 'description:string'],
+  "MAPS": ['event_id:int', 'page_index:int', 'list_index:int', 'text'],
+  "Skills.json": ['id:int', 'name:string', 'description:string', 'message1:string', 'message2:string'],
+  "States.json": ['id:int', 'name:string', 'note:string', 'message1:string', 'message2:string', 'message3:string', 'message4:string']
+}
 
 function Archivo(name, json, selected, highlighted){
-  this.id = parseInt(name.match(/\d+/)[0]);
+  //this.id = parseInt(name.match(/\d+/)[0]);
   this.name = name;
   this.json = json;
   this.selected = selected || false;
   this.highlighted = highlighted || false;
 
   try{
-    this.header = keysForFile[name.match(/Map[0-9]{3}.json/) !== null ? 'MAPS' : this.name]['keys'];  
+    this.header = keysForFile[name.match(/Map[0-9]{3}.json/) !== null ? 'MAPS' : this.name];
   }catch{ console.log(`No header for ${name}`); }  
 
-  try{
+  //try{
     this.tsv = this.toTSV();
-  }catch{ console.log(`Unable to generate TSV for ${name}`); }  
+  //}catch{ console.log(`Unable to generate TSV for ${name}`); }  
 }
 
 Archivo.prototype.toTSV = function(){
   if(this.header === undefined){
     console.log(`Unable to generate TSV for ${name} withtout header`);   
   }else{
-    let rows = []
-    this.json.events.filter((row) => row!==null).forEach((row) => {
-      row.pages.forEach((page, page_index) => {
-        page.list?.forEach((list, list_index) => {
-          if(list.code === 401){
-            text = list.parameters[0].replace(/\\/g, '\\\\').replace(/\"/g, '\\\"');
-            rows.push([row.id, page_index, list_index, text].join('\t'))
-          }else if(list.code === 101 && ['Portrait_Recruits2', 'Portrait_NPCs'].includes(list.parameters[0])){
-            text = list.parameters[4].replace(/\\/g, '\\\\').replace(/\"/g, '\\\"');
-            rows.push([row.id, page_index, list_index, text].join('\t'))
-          }
+    if(this.name.match(/Map[0-9]{3}.json/)){
+      let rows = []
+      this.json.events.filter((row) => row!==null).forEach((row) => {
+        row.pages.forEach((page, page_index) => {
+          page.list?.forEach((list, list_index) => {
+            if(list.code === 401){
+              text = list.parameters[0].replace(/\\/g, '\\\\').replace(/\"/g, '\\\"');
+              rows.push([row.id, page_index, list_index, text].join('\t'))
+            }else if(list.code === 101 && ['Portrait_Recruits2', 'Portrait_NPCs'].includes(list.parameters[0])){
+              text = list.parameters[4].replace(/\\/g, '\\\\').replace(/\"/g, '\\\"');
+              rows.push([row.id, page_index, list_index, text].join('\t'))
+            }
+          })
         })
-      })
-    });
+      });
 
-    let pre_tsv = [this.header.join('\t'), rows.join('\r\n')].join('\r\n');
+      let pre_tsv = [this.header.map((key) => key.split(':')[0]).join('\t'), rows.join('\r\n')].join('\r\n');
 
-    this.tsv = pre_tsv;
-    this.rows = rows.length;
+      this.tsv = pre_tsv;
+      this.rows = rows.length;
 
-    return this.tsv;
+      return this.tsv;
+    }else{
+      let rows = []
+      this.json.filter((row) => row!==null).forEach((row) => {
+        //if(row.name){rows.push([row.id, row.name].join('\t'))};
+        console.log(row);
+        formatted_row = this.header.map((key) => key.split(':')[1] === 'int' ? row[key.split(':')[0]] : row[key.split(':')[0]].replace(/\n/g, '\\n'));
+        if(!formatted_row.slice(1).every(elem => elem === null || elem === ''))
+          rows.push(formatted_row.join('\t'));
+      });
+
+      let pre_tsv = [this.header.map((key) => key.split(':')[0]).join('\t'), rows.join('\r\n')].join('\r\n');
+
+      this.tsv = pre_tsv;
+      this.rows = rows.length;
+
+      return this.tsv;
+    }
+  }
+}
+
+Archivo.prototype.mergeTranslation = function(){
+  const file = this;
+  var exportJson = this.json;
+  if(this.name.match(/Map[0-9]{3}.json/)){
+    document.getElementById('console').value = '';
+    const translations = document.getElementById('merge-input').value
+
+    translations.split('\n').slice(1).forEach(function(row, index){
+      row = row.split('\t').map((col, idx) => file.header[idx].split(':')[1] === 'int' ? parseInt(col) : col);
+      
+      try{
+        if (['Portrait_Recruits2', 'Portrait_NPCs'].includes(exportJson.events.find((event) => event?.id === row[0])?.pages[row[1]]?.list[row[2]]?.parameters[0])){
+          exportJson.events.find((event) => event?.id === row[0]).pages[row[1]].list[row[2]].parameters[4] = row[3].replace(/\\\\/g, '\\').replace(/\\\"/g, '\"');
+        }else{
+          exportJson.events.find((event) => event?.id === row[0]).pages[row[1]].list[row[2]].parameters = [row[3].replace(/\\\\/g, '\\').replace(/\\\"/g, '\"')];
+        }
+      }catch(e){
+        console.log(`translation:${index + 1} - line skipped [${row}]`);
+      }
+    })
+    return exportJson;
+  }else{
+    document.getElementById('console').value = '';
+    const translations = document.getElementById('merge-input').value
+
+    translations.split('\n').slice(1).forEach(function(row, index){
+      row = row.split('\t').map((col, idx) => file.header[idx].split(':')[1] === 'int' ? parseInt(col) : col);
+      
+      try{
+        file.header.slice(1).forEach((key, idx) => {
+          exportJson.find((line) => line?.id === row[0])[key.split(':')[0]] = row[idx+1].replace(/\\\\/g, '\\').replace(/\\\"/g, '\"').replace(/\\n/g, '\n');
+        })        
+      }catch(e){
+        console.log(`translation:${index + 1} - line skipped [${row}]`);
+      }
+    })
+    return exportJson;
   }
 }
 
@@ -58,8 +126,8 @@ function fileSelected(event) {
 };
 
 function updateFileExplorer(){
-  allFiles = allFiles.sort((a, b) => a.id - b.id);
-  if(allFiles.filter((file) => file.highlighted).length < 1){
+  allFiles = allFiles.sort((a, b) => a.name.localeCompare(b.name));
+  if(!allFiles.filter((file) => file.highlighted)[0] && allFiles.filter((file) => file.rows > 0)[0]){
     allFiles.filter((file) => file.rows > 0)[0].highlighted = true;
   }
   document.getElementById('files-list').innerHTML = '';
@@ -67,7 +135,7 @@ function updateFileExplorer(){
   allFiles.forEach((row) => {
     table_row = document.createElement('tr');
     if(row.rows > 0){
-      table_row.innerHTML += `<td scope="row"><input class="form-check-input" type="checkbox" ${row.selected ? 'checked' : ''} id="${row.id}" value="${row.name}"></td>`
+      table_row.innerHTML += `<td scope="row"><input class="form-check-input" type="checkbox" ${row.selected ? 'checked' : ''} id="${row.name}" value="${row.name}"></td>`
     }else{
       table_row.innerHTML = '<td scope="row"></td>';  
     }
@@ -97,12 +165,12 @@ function selectAll(){
 
 function clickTableRow(elem){
   if(event.target.classList.contains('form-check-input')){
-    data = allFiles.filter((file) => file.id === parseInt(elem.target.parentNode.querySelector('input[type=checkbox]').id))[0];
+    data = allFiles.filter((file) => file.name === elem.target.parentNode.querySelector('input[type=checkbox]').id)[0];
     data.selected = !data.selected;
     enableDownloadTSVButton();
   }else if(!elem.target.parentNode.classList.contains('table-primary')){
     allFiles.filter((file) => file.highlighted).map((file) => file.highlighted = false );
-    data = allFiles.filter((file) => file.rows > 0).filter((file) => file.id === parseInt(elem.target.parentNode.querySelector('input[type=checkbox]')?.id))[0];
+    data = allFiles.filter((file) => file.rows > 0).filter((file) => file.name === elem.target.parentNode.querySelector('input[type=checkbox]')?.id)[0];
     if(!data) return;
     data.highlighted = true;
     document.getElementById('output').value = data.tsv;
@@ -139,151 +207,27 @@ function downloadBlob(content, filename, type) {
   dlAnchorElem.click();
 }
 
-let jsonFile, fileHeader, FileName, exportFile;
-const keysForFile = { 
-  "Items.json": { 
-    'keys': ['id', 'description', 'name'] 
-  },
-  "MAPS": { 
-    'lookup': {
-      'id': ['events'],
-      'parameters': ['events', 'pages', 'list@code=401']
-    }, 
-    'keys': ['event_id', 'page_index', 'list_index', 'text'] 
-  },
-  "Enemies.json": ['id', 'name'] 
-}
-
-function reset(){
-  document.getElementById('controls').innerHTML = '';
-  document.getElementById('output').innerHTML = '';
-  fileHeader = null;
-}
-
-
-
-function onReaderLoad(event){
-  jsonFile = JSON.parse(event.target.result);
-  loadCsv();
-}
-
-function setHeader(){
-  fileHeader = [...document.querySelectorAll('#controls > input[type=checkbox]:checked')].map((elem) => elem.id)
-  loadCsv();
-}
-
-function loadCsv(file){
-  const items = file || jsonFile
-  
-  if(!fileHeader){ setHeader(); }
-  else{
-    const header = fileHeader;
-    const replacer = (key, value) => value === null ? '' : value
-
-    rows = []
-    items.events.filter((row) => row!==null).forEach((row) => {
-      row.pages.forEach((page, page_index) => {
-        page.list?.forEach((list, list_index) => {
-          if(list.code === 401){
-            text = list.parameters[0].replace(/\\/g, '\\\\');
-            rows.push([row.id, page_index, list_index, text].join('\t'))
-          }else if(list.code === 101 && ['Portrait_Recruits2', 'Portrait_NPCs'].includes(list.parameters[0])){
-            text = list.parameters[4].replace(/\\/g, '\\\\');
-            rows.push([row.id, page_index, list_index, text].join('\t'))
-          }
-        })
-      })
-    });
-
-    const csv = [header.join('\t'), rows.join('\r\n')].join('\r\n');
-
-    document.querySelector('.row-number').innerHTML = `${rows.length} rows`
-    document.getElementById('output').value = csv;
-    return csv;
-  }
-}
-
-function mergeFile(){
-  document.getElementById('console').value = '';
-  exportFile = allFiles.filter((file) => file.highlighted)[0];
-  const translations = document.getElementById('merge-input').value
-
-  translations.split('\n').slice(1).forEach(function(row, index){
-    row = row.split('\t').map((col, index) => index > 2 ? col: parseInt(col));
-    
-    try{
-      if (['Portrait_Recruits2', 'Portrait_NPCs'].includes(exportFile.json.events.find((event) => event?.id === row[0])?.pages[row[1]]?.list[row[2]]?.parameters[0])){
-        exportFile.json.events.find((event) => event?.id === row[0]).pages[row[1]].list[row[2]].parameters[4] = row[3].replace(/\\\\/g, '\\').replace(/\\\"/g, '\"');
-      }else{
-        exportFile.json.events.find((event) => event?.id === row[0]).pages[row[1]].list[row[2]].parameters = [row[3].replace(/\\\\/g, '\\').replace(/\\\"/g, '\"')];
-      }
-    }catch(e){
-      console.log(`Skipped line ${index} invalid data: ${row}`);
-    }
-  })
-  return exportFile.json;
-}
-
 (()=>{
   const console_log = window.console.log;
   window.console.log = function(...args){
     console_log(...args);
     var textarea = document.getElementById('console');
     if(!textarea) return;
-    args.forEach(arg => textarea.value = `${JSON.stringify(arg)}\n${textarea.value}`);
+    args.forEach(arg => textarea.value = `${JSON.stringify(arg).replace(/"/g, '')}\n${textarea.value}`);
   }
 })();
 
 function activateMergeButton(){
   document.getElementById('merge-button').disabled = !(document.getElementById('merge-input').value.length > 0 && document.getElementById('json-file-input').files.length > 0);
 }
-/* NEW STUFF */
-
-/*function fileSelected(event) {
-  const files = document.getElementById('json-file-input').files;
-  Array.from(files).forEach(file => {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      downloadBlob(toTSV(JSON.parse(reader.result), file.name), file.name, 'tsv');
-    }
-    reader.readAsText(file);
-  })
-};
-*/
-function toTSV(file, name){
-  file_key = name.match(/Map[0-9]{3}.json/) !== null ? 'MAPS' : name
-  fileHeader = keysForFile[file_key]['keys']
-
-  rows = []
-  file.events.filter((row) => row!==null).forEach((row) => {
-    row.pages.forEach((page, page_index) => {
-      page.list?.forEach((list, list_index) => {
-        if(list.code === 401){
-          text = list.parameters[0].replace(/\\/g, '\\\\');
-          rows.push([row.id, page_index, list_index, text].join('\t'))
-        }else if(list.code === 101 && ['Portrait_Recruits2', 'Portrait_NPCs'].includes(list.parameters[0])){
-          text = list.parameters[4].replace(/\\/g, '\\\\');
-          rows.push([row.id, page_index, list_index, text].join('\t'))
-        }
-      })
-    })
-  });
-
-  let tsv = [fileHeader.join('\t'), rows.join('\r\n')].join('\r\n');
-
-  document.querySelector('.row-number').innerHTML = `${rows.length} rows`
-  document.getElementById('output').value = tsv;
-
-  return tsv;
-}
-
 
 document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById('json-file-input').addEventListener('change', fileSelected);
   document.querySelector('button.reload').addEventListener('click', fileSelected);
   document.getElementById('merge-input').addEventListener('input', activateMergeButton);
   document.getElementById('merge-button').addEventListener('click', function(){
-    downloadBlob(mergeFile(), allFiles.filter((file) => file.highlighted)[0].name, 'json');
+    const selectedFile = allFiles.filter((file) => file.highlighted)[0]
+    downloadBlob(selectedFile.mergeTranslation(), selectedFile.name, 'json');
   });
   document.getElementById('selectAll').addEventListener('change', selectAll);
   document.getElementById('download-tsv-button').addEventListener('click', downloadFiles);
